9a82ab8d77d8a586ce88d9670d8eb49c703a0c8f
Sönke Holz <sholz8530@gmail.com>
2024-07-03 20:19:48 +0200

    Kernel: Move boot info variables into a shared struct

---
+++ Kernel/Prekernel/Arch/aarch64/ArchSpecificBootInfo.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024, Sönke Holz <sholz8530@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Types.h>
+
+namespace Kernel {
+
+struct ArchSpecificBootInfo {
+};
+
+}
---
+++ Kernel/Prekernel/Arch/riscv64/ArchSpecificBootInfo.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024, Sönke Holz <sholz8530@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <AK/Types.h>
+
+namespace Kernel {
+
+struct ArchSpecificBootInfo {
+    FlatPtr boot_hart_id;
+};
+
+}
