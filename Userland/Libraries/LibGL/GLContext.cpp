82a11c9bdd5b6d3e32df9f3bbaf0d177beede99a
Sönke Holz <soenke.holz@serenityos.org>
2026-04-29 20:06:24 +0200

    WIP: Make LibGL GPU driver configurable

--- Userland/Libraries/LibGL/GLContext.cpp
+++ Userland/Libraries/LibGL/GLContext.cpp
@@ -9,6 +9,7 @@
 #include <AK/Debug.h>
 #include <AK/StringBuilder.h>
 #include <AK/Vector.h>
+#include <LibCore/Environment.h>
 #include <LibGL/GLContext.h>
 #include <LibGL/Image.h>
 #include <LibGPU/Device.h>
@@ -881,8 +882,7 @@ ErrorOr<ByteBuffer> GLContext::build_extension_string()
 
 ErrorOr<NonnullOwnPtr<GLContext>> create_context(Gfx::Bitmap& bitmap)
 {
-    // FIXME: Make driver selectable. This is currently hardcoded to LibSoftGPU
-    auto driver = TRY(GPU::Driver::try_create("softgpu"sv));
+    auto driver = TRY(GPU::Driver::try_create(Core::Environment::get("LIBGL_GPU_DRIVER"sv).value_or("v3dgpu"sv)));
     auto device = TRY(driver->try_create_device(bitmap.size()));
     auto context = make<GLContext>(driver, move(device), bitmap);
     dbgln_if(GL_DEBUG, "GL::create_context({}) -> {:p}", bitmap.size(), context.ptr());
