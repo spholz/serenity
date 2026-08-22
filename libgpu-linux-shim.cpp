4d8e3a4c00079102a9012bc147e74a98f3445095
Sönke Holz <soenke.holz@serenityos.org>
2026-08-22 16:41:07 +0200

    WIP: Update Linux shim to new API

--- libgpu-linux-shim.cpp
+++ libgpu-linux-shim.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+#include <AK/Format.h>
 #include <AK/Vector.h>
 #include <Kernel/API/V3D.h>
 
@@ -11,9 +12,23 @@
 #include <drm/v3d_drm.h>
 #include <xf86drm.h>
 
-static Vector<u32> s_buffer_object_handles;
+static constexpr auto SERENITY_DEVICE_PATH = "/dev/gpu/render0";
+static constexpr auto LINUX_DEVICE_PATH = "/dev/dri/renderD128";
+
+// Used to translate the Serenity kernel API to the Linux DRM API.
+struct Buffer {
+    int fd;
+    u32 drm_buffer_object_handle;
+    u32 gpu_virtual_address;
+    u64 drm_mmap_offset;
+};
+static Vector<Buffer> s_buffers;
+
+static Vector<u32> s_all_drm_buffer_object_handles;
 
 static int (*original_ioctl)(int fd, unsigned long request, ...) = nullptr;
+static void* (*original_mmap)(void* addr, size_t length, int prot, int flags, int fd, off_t offset) = nullptr;
+static int (*original_openat)(int dirfd, char const* path, int flags, ...) = nullptr;
 
 static Optional<u32> s_out_sync;
 
@@ -52,8 +67,8 @@ static int ioctl_v3d_submit_job(int fd, V3DJob const* job)
         .qts = job->tile_state_data_array_base_address,
 
         // BOs referenced by this job
-        .bo_handles = bit_cast<u64>(s_buffer_object_handles.data()),
-        .bo_handle_count = static_cast<u32>(s_buffer_object_handles.size()),
+        .bo_handles = bit_cast<u64>(s_all_drm_buffer_object_handles.data()),
+        .bo_handle_count = static_cast<u32>(s_all_drm_buffer_object_handles.size()),
 
         .flags = 0,
 
@@ -87,10 +102,9 @@ static int ioctl_v3d_allocate_buffer(int fd, V3DBuffer* buffer)
     if (drmIoctl(fd, DRM_IOCTL_V3D_CREATE_BO, &create_bo) != 0)
         return -1;
 
-    s_buffer_object_handles.append(create_bo.handle);
+    buffer->gpu_virtual_address = create_bo.offset;
 
-    buffer->id = create_bo.handle;
-    buffer->address = create_bo.offset;
+    s_all_drm_buffer_object_handles.append(create_bo.handle);
 
     struct drm_v3d_mmap_bo mmap_bo = {
         .handle = create_bo.handle,
@@ -103,22 +117,50 @@ static int ioctl_v3d_allocate_buffer(int fd, V3DBuffer* buffer)
     if (drmIoctl(fd, DRM_IOCTL_V3D_MMAP_BO, &mmap_bo) != 0)
         return -1;
 
-    buffer->mmap_offset = mmap_bo.offset;
+    s_buffers.append({
+        .fd = fd,
+        .drm_buffer_object_handle = create_bo.handle,
+        .gpu_virtual_address = create_bo.offset,
+        .drm_mmap_offset = mmap_bo.offset,
+    });
 
     return 0;
 }
 
-static int ioctl_v3d_free_buffer(int fd, u64 buffer_id)
+static Optional<size_t> look_up_buffer_index(int fd, FlatPtr gpu_virtual_address)
 {
+    return s_buffers.find_first_index_if([gpu_virtual_address, fd](Buffer const& buffer) {
+        return buffer.fd == fd && buffer.gpu_virtual_address == gpu_virtual_address;
+    });
+}
+
+static Optional<Buffer const&> look_up_buffer(int fd, FlatPtr gpu_virtual_address)
+{
+    auto buffer_index = look_up_buffer_index(fd, gpu_virtual_address);
+    if (!buffer_index.has_value())
+        return {};
+
+    return s_buffers[*buffer_index];
+}
+
+static int ioctl_v3d_free_buffer(int fd, FlatPtr gpu_virtual_address)
+{
+    auto buffer_index = look_up_buffer_index(fd, gpu_virtual_address);
+    if (!buffer_index.has_value())
+        return EINVAL;
+
+    auto const& buffer = s_buffers[*buffer_index];
+
     struct drm_gem_close close = {
-        .handle = static_cast<u32>(buffer_id),
+        .handle = static_cast<u32>(buffer.drm_buffer_object_handle),
         .pad = 0,
     };
 
+    s_all_drm_buffer_object_handles.remove_first_matching([&buffer](u32 handle) { return handle == buffer.drm_buffer_object_handle; });
+    s_buffers.remove(*buffer_index);
+
     if (drmIoctl(fd, DRM_IOCTL_GEM_CLOSE, &close) != 0)
         return -1;
-
-    s_buffer_object_handles.remove_first_matching([buffer_id](u32 handle) { return handle == buffer_id; });
 
     return 0;
 }
@@ -144,6 +186,47 @@ extern "C" int ioctl(int fd, unsigned long request, ...)
         ret = ioctl_v3d_submit_job(fd, reinterpret_cast<V3DJob const*>(arg));
     else
         ret = original_ioctl(fd, request, arg);
+
+    va_end(args);
+
+    return ret;
+}
+
+extern "C" void* mmap64(void* addr, size_t length, int prot, int flags, int fd, off_t offset)
+{
+    if (original_mmap == nullptr) {
+        original_mmap = reinterpret_cast<decltype(original_mmap)>(dlsym(RTLD_NEXT, "mmap"));
+        VERIFY(original_mmap != nullptr);
+    }
+
+    auto maybe_buffer = look_up_buffer(fd, offset);
+    if (!maybe_buffer.has_value()) {
+        return original_mmap(addr, length, prot, flags, fd, offset);
+    }
+
+    auto const& buffer = maybe_buffer.value();
+
+    return original_mmap(addr, length, prot, flags, fd, buffer.drm_mmap_offset);
+}
+
+extern "C" int openat64(int dirfd, char const* path, int flags, ...)
+{
+    if (original_openat == nullptr) {
+        original_openat = reinterpret_cast<decltype(original_openat)>(dlsym(RTLD_NEXT, "openat"));
+        VERIFY(original_openat != nullptr);
+    }
+
+    va_list args;
+    va_start(args, flags);
+
+    mode_t mode = va_arg(args, mode_t);
+
+    int ret = -1;
+    if (strcmp(path, SERENITY_DEVICE_PATH) == 0) {
+        ret = original_openat(dirfd, LINUX_DEVICE_PATH, flags, mode);
+    } else {
+        ret = original_openat(dirfd, path, flags, mode);
+    }
 
     va_end(args);
 
