2b5ac689e62d6540b78d7872aa80e110126eef7b
Sönke Holz <soenke.holz@serenityos.org>
2026-08-25 13:09:56 +0200

    LibIPC: Allow overriding the session ID via an environment variable
    This makes it possible to start GUI apps from contexts with a
    session ID that is not equal to the logged in the desktop session.
    This allows starting GUI apps via SSH if SSHServer was started by
    SystemServer.
    
    XXX: Doesn't work because unveil()s sometimes also use the session id.

--- Userland/Libraries/LibIPC/ConnectionToServer.h
+++ Userland/Libraries/LibIPC/ConnectionToServer.h
@@ -6,23 +6,25 @@
 
 #pragma once
 
+#include <LibCore/Environment.h>
 #include <LibCore/SessionManagement.h>
 #include <LibIPC/Connection.h>
 
 namespace IPC {
 
-#define IPC_CLIENT_CONNECTION(klass, socket_path)                                                      \
-    C_OBJECT_ABSTRACT(klass)                                                                           \
-public:                                                                                                \
-    template<typename Klass = klass, class... Args>                                                    \
-    static ErrorOr<NonnullRefPtr<klass>> try_create(Args&&... args)                                    \
-    {                                                                                                  \
-        auto parsed_socket_path = TRY(Core::SessionManagement::parse_path_with_sid(socket_path));      \
-        auto socket = TRY(Core::LocalSocket::connect(move(parsed_socket_path)));                       \
-        /* We want to rate-limit our clients */                                                        \
-        TRY(socket->set_blocking(true));                                                               \
-                                                                                                       \
-        return adopt_nonnull_ref_or_enomem(new (nothrow) Klass(move(socket), forward<Args>(args)...)); \
+#define IPC_CLIENT_CONNECTION(klass, socket_path)                                                                    \
+    C_OBJECT_ABSTRACT(klass)                                                                                         \
+public:                                                                                                              \
+    template<typename Klass = klass, class... Args>                                                                  \
+    static ErrorOr<NonnullRefPtr<klass>> try_create(Args&&... args)                                                  \
+    {                                                                                                                \
+        auto forced_session_id = forced_session_id_from_environment();                                               \
+        auto parsed_socket_path = TRY(Core::SessionManagement::parse_path_with_sid(socket_path, forced_session_id)); \
+        auto socket = TRY(Core::LocalSocket::connect(move(parsed_socket_path)));                                     \
+        /* We want to rate-limit our clients */                                                                      \
+        TRY(socket->set_blocking(true));                                                                             \
+                                                                                                                     \
+        return adopt_nonnull_ref_or_enomem(new (nothrow) Klass(move(socket), forward<Args>(args)...));               \
     }
 
 template<typename ClientEndpoint, typename ServerEndpoint>
@@ -43,6 +45,22 @@ public:
     {
         // Override this function if you don't want your app to exit if it loses the connection.
         exit(0);
+    }
+
+protected:
+    static Optional<pid_t> forced_session_id_from_environment()
+    {
+        auto forced_session_id_string = Core::Environment::get("IPC_SESSION_ID"sv);
+        if (!forced_session_id_string.has_value())
+            return {};
+
+        auto forced_session_id = forced_session_id_string.value().to_number<pid_t>();
+        if (!forced_session_id.has_value()) {
+            dbgln("LibIPC: Invalid session ID from environment: \"{}\"", forced_session_id_string);
+            return {};
+        }
+
+        return forced_session_id;
     }
 };
 
